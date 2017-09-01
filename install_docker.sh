b5edb2eb1209017ee9a8875a75b7c3282d2d6da2
Nathan Kinkade <nath@nkinka.de>
2017-09-01 14:53:25 -0600

    Removes sudo command from startup script a) because sudo isn't installed b) because the script runs as root.

--- install_docker.sh
+++ install_docker.sh
@@ -13,7 +13,7 @@ apt-get install --yes \
 
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 
-sudo add-apt-repository \
+add-apt-repository \
   "deb [arch=amd64] https://download.docker.com/linux/debian \
   $(lsb_release -cs) \
   stable"
