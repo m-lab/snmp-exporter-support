m-lab/snmp-exporter-support

commit ba2d1809e93f7afdd5682ff4da092993b3da87a1

Causes deployment to create the GCE instance automatically, rather than having to create it in advance manually.

deploy_snmp_exporter.sh
@@ -4,16 +4,15 @@ set -e
 set -u
 set -x
 
-# These variables will likely not change.
+# These variables should not change much
 USAGE="Usage: $0 <project>"
 PROJECT=${1:?Please provide project name: $USAGE}
 CREDS_FILE="snmp-exporter-service-account.json"
 SCP_FILES="Dockerfile mlab.yml"
-IMAGE_TAG="m-lab/prometheus-snmp-exporter"
-
-# These variables will change depending on the GCE instance created.
-GCE_INSTANCE="kinkade-snmp-exporter"
+EXPORTER_URI=$(cut -d' ' -f2 $TRAVIS_BUILD_DIR/Dockerfile)
 GCE_ZONE="us-central1-a"
+GCE_NAME="snmp-exporter"
+GCE_IP_NAME="snmp-exporter-public-ip"
 
 # Add gcloud to PATH.
 source "${HOME}/google-cloud-sdk/path.bash.inc"
@@ -41,17 +40,30 @@ for scp_file in ${SCP_FILES}; do
   fi
 done
 
-# Copy required snmp_exporter files to the GCE instance
-gcloud compute scp $SCP_FILES $GCE_INSTANCE:~
+# Delete the existing GCE instance, if it exists. gcloud has an exit status of 0
+# whether any instances are found or not. When no instances are found, a short
+# message is echoed to stderr. When an instance is found a summary is echoed to
+# stdout. If $EXISTING_INSTANCE is not null then we infer that the instance
+# already exists.
+EXISTING_INSTANCE=$(gcloud compute instances list --filter "name=${GCE_NAME}")
+if [[ -n "$INST" ]]; then
+  gcloud compute instances delete $GCE_NAME --quiet
+fi
 
-# Build the snmp_exporter Docker container
-gcloud compute ssh $GCE_INSTANCE --command "sudo docker build -t ${IMAGE_TAG} ."
+# Create the new GCE instance. NOTE: $GCE_IP_NAME *must* refer to an existing
+# static external IP address for the project.
+gcloud compute instances create $GCE_NAME --address $GCE_IP_NAME
 
-# Delete any existing snmp_exporter containters
-gcloud compute ssh $GCE_INSTANCE --command \
-    "if [[ -n \"\$(sudo docker ps -q -f=ancestor=$IMAGE_TAG)\" ]]; then \
-    sudo docker rm -f \$(sudo docker ps -q -f=ancestor=$IMAGE_TAG); fi"
+# Copy required snmp_exporter files to the GCE instance.
+gcloud compute scp $SCP_FILES $GCE_NAME:~
+
+# Build the snmp_exporter Docker container.
+gcloud compute ssh $GCE_NAME --command "sudo docker build ."
+
+# Delete any existing snmp_exporter containters.
+gcloud compute ssh $GCE_NAME --command \
+  "sudo docker rm -f \$(sudo docker ps -q -f=ancestor=$EXPORTER_URI)"
 
 # Start a new container based on the new/updated image
-gcloud compute ssh $GCE_INSTANCE --command \
-  "sudo docker run -p 9116:9116 -d ${IMAGE_TAG}"
+gcloud compute ssh $GCE_NAME --command \
+  "sudo docker run -p 9116:9116 -d ${EXPORTER_URI}"

commit 2b797f05a9c89e04f74d2351583913fceb0b34a6

Fixes another regression from botched commit @ba2d180 which accidentally overwrote wanted changes.

deploy_snmp_exporter.sh
@@ -9,7 +9,7 @@ USAGE="Usage: $0 <project>"
 PROJECT=${1:?Please provide project name: $USAGE}
 CREDS_FILE="snmp-exporter-service-account.json"
 SCP_FILES="Dockerfile mlab.yml"
-EXPORTER_URI=$(grep '^FROM' $TRAVIS_BUILD_DIR/Dockerfile | cut -d' ' -f2)
+IMAGE_TAG="m-lab/prometheus-snmp-exporter"
 GCE_ZONE="us-central1-a"
 GCE_NAME="snmp-exporter"
 GCE_IP_NAME="snmp-exporter-public-ip"
@@ -64,9 +64,10 @@ gcloud compute scp $SCP_FILES $GCE_NAME:~
 gcloud compute ssh $GCE_NAME --command "sudo docker build ."
 
 # Delete any existing snmp_exporter containters.
-gcloud compute ssh $GCE_NAME --command \
-  "sudo docker rm -f \$(sudo docker ps -q -f=ancestor=$EXPORTER_URI)"
+gcloud compute ssh $GCE_INSTANCE --command \
+  "if [[ -n \"\$(sudo docker ps -q -f=ancestor=$IMAGE_TAG)\" ]]; then \
+  sudo docker rm -f \$(sudo docker ps -q -f=ancestor=$IMAGE_TAG); fi"
 
 # Start a new container based on the new/updated image
 gcloud compute ssh $GCE_NAME --command \
-  "sudo docker run -p 9116:9116 -d ${EXPORTER_URI}"
+  "sudo docker run -p 9116:9116 -d ${IMAGE_TAG}"