m-lab/snmp-exporter-support

commit ba2d1809e93f7afdd5682ff4da092993b3da87a1

Causes deployment to create the GCE instance automatically, rather than having to create it in advance manually.

deploy_snmp_exporter.sh
@@ -4,16 +4,15 @@ set -e
 set -u
 set -x
 
-# These variables will likely not change.
+# These variables should not change much
 USAGE="Usage: $0 <project>"
 PROJECT=${1:?Please provide project name: $USAGE}
 CREDS_FILE="snmp-exporter-service-account.json"
 SCP_FILES="Dockerfile mlab.yml"
-IMAGE_TAG="m-lab/prometheus-snmp-exporter"
-
-# These variables will change depending on the GCE instance created.
-GCE_INSTANCE="kinkade-snmp-exporter"
+EXPORTER_URI=$(cut -d' ' -f2 $TRAVIS_BUILD_DIR/Dockerfile)
 GCE_ZONE="us-central1-a"
+GCE_NAME="snmp-exporter"
+GCE_IP_NAME="snmp-exporter-public-ip"
 
 # Add gcloud to PATH.
 source "${HOME}/google-cloud-sdk/path.bash.inc"
@@ -41,17 +40,30 @@ for scp_file in ${SCP_FILES}; do
   fi
 done
 
-# Copy required snmp_exporter files to the GCE instance
-gcloud compute scp $SCP_FILES $GCE_INSTANCE:~
+# Delete the existing GCE instance, if it exists. gcloud has an exit status of 0
+# whether any instances are found or not. When no instances are found, a short
+# message is echoed to stderr. When an instance is found a summary is echoed to
+# stdout. If $EXISTING_INSTANCE is not null then we infer that the instance
+# already exists.
+EXISTING_INSTANCE=$(gcloud compute instances list --filter "name=${GCE_NAME}")
+if [[ -n "$INST" ]]; then
+  gcloud compute instances delete $GCE_NAME --quiet
+fi
 
-# Build the snmp_exporter Docker container
-gcloud compute ssh $GCE_INSTANCE --command "sudo docker build -t ${IMAGE_TAG} ."
+# Create the new GCE instance. NOTE: $GCE_IP_NAME *must* refer to an existing
+# static external IP address for the project.
+gcloud compute instances create $GCE_NAME --address $GCE_IP_NAME
 
-# Delete any existing snmp_exporter containters
-gcloud compute ssh $GCE_INSTANCE --command \
-    "if [[ -n \"\$(sudo docker ps -q -f=ancestor=$IMAGE_TAG)\" ]]; then \
-    sudo docker rm -f \$(sudo docker ps -q -f=ancestor=$IMAGE_TAG); fi"
+# Copy required snmp_exporter files to the GCE instance.
+gcloud compute scp $SCP_FILES $GCE_NAME:~
+
+# Build the snmp_exporter Docker container.
+gcloud compute ssh $GCE_NAME --command "sudo docker build ."
+
+# Delete any existing snmp_exporter containters.
+gcloud compute ssh $GCE_NAME --command \
+  "sudo docker rm -f \$(sudo docker ps -q -f=ancestor=$EXPORTER_URI)"
 
 # Start a new container based on the new/updated image
-gcloud compute ssh $GCE_INSTANCE --command \
-  "sudo docker run -p 9116:9116 -d ${IMAGE_TAG}"
+gcloud compute ssh $GCE_NAME --command \
+  "sudo docker run -p 9116:9116 -d ${EXPORTER_URI}"

commit fdfbfea63d40525a0c1f21d34aae51da35337801

Auto-deploy valid snmp_exporter configs to M-Lab GCP GCE instances (#2)
* Initial, still very rough and partially broken, commit for deloying snmp_exporter to a GCE instance.

* List prerequisites in README.md.

* Grabs the snmp_exporter URI and version from the Dockerfile.

* Skip the conditional Travis-CI deployment, at least for now.

* Explicity adds Google Cloud SDK dirs to PATH to avoid 'gcloud: command not found' errors in the deploy script.

* Use absolute paths for script and file locations.

* Uses TRAVIS_BUILD_DIR instead of HOME for abs paths.

* Looks for Google service account creds in HOME, not TRAVIS_BUILD_DIR.

* Fixes file test to look in right directory.

* Ugh. Sets path for GCP service account creds file to HOME, again.

* Looks for GCP service account creds in /tmp.

* Explicity decrypts the GCP creds file to /tmp.

* Provides an explicit path to the encrypted file to be decrypted.

* Sets all_branches:true to trigger builds on the dev branch.

* Puts 'all_branches' flag in the right location, in the 'on' section.

* Limits EXPORTER_URI to just the line in Dockerfile that contains FROM.

* Cache ~/.ssh/, and don't try to remove containers that don't exist.

* Removes quotes from wrapped text.

* Tags the Docker build appropriately, and uses that tag to refer to image and containers.

* Causes deployment to create the GCE instance automatically, rather than having to create it in advance manually.

* Corrects a variable name spelling.

* Adds a startup script for the GCE instance that will install Docker.

* Adds the actual startup script to install Docker.

* Sets apt commands in install_docker.sh to automatially assume 'yes' to any prompts.

* Removes sudo command from startup script a) because sudo isn't installed b) because the script runs as root.

* Adds missing line continuation character.

* Forcefully restarts docker after the install.

* Use CoreOS base image instead of Debian so that we get docker be default instead of having to install it.

* Reimplements commit @57a69c9, which somehow got removed along the way.

* Fixes another regression from botched commit @ba2d180 which accidentally overwrote wanted changes.

.gitignore
@@ -1,0 +1,2 @@
+mlab.yml
+.coverage

.gitmodules
@@ -1,0 +1,9 @@
+[submodule "switch-config"]
+	path = switch-config
+	url = git@github.com:m-lab/switch-config.git
+[submodule "travis"]
+	path = travis
+	url = http://github.com/m-lab/travis.git
+[submodule "git-hooks"]
+	path = git-hooks
+	url = https://github.com/m-lab/git-hooks.git

.travis.yml
@@ -1,0 +1,46 @@
+language: python
+
+before_install:
+  - $TRAVIS_BUILD_DIR/travis/decrypt.sh
+    "$encrypted_d0f94f538e88_key"
+    "$encrypted_d0f94f538e88_iv"
+    $TRAVIS_BUILD_DIR/snmp-exporter-service-accounts.tar.enc
+    $TRAVIS_BUILD_DIR/snmp-exporter-service-accounts.tar
+    /tmp
+
+install:
+  - pip install -r test-requirements.txt
+
+script:
+  - coverage run --source='.' -m unittest discover --pattern='*_test.py'
+  - coverage report
+  - "$TRAVIS_BUILD_DIR/travis/install_gcloud.sh"
+
+cache:
+  directories:
+    - "$HOME/google-cloud-sdk/"
+
+deploy:
+  # Sandbox - conditionally deploy to GCP project mlab-sandbox
+  - provider: script
+    script: "$TRAVIS_BUILD_DIR/deploy_snmp_exporter.sh mlab-sandbox"
+    skip_cleanup: true
+    on:
+      repo: m-lab/prometheus-snmp-exporter
+      branch: sandbox-*
+      condition: "$TRAVIS_EVENT_TYPE == push"
+  # Staging - conditionally deploy to GCP project mlab-staging
+  - provider: script
+    script: "$TRAVIS_BUILD_DIR/deploy_snmp_exporter.sh mlab-staging"
+    skip_cleanup: true
+    on:
+      repo: m-lab/prometheus-snmp-exporter
+      branch: master
+      condition: "$TRAVIS_EVENT_TYPE == push"
+  # Production - conditionally deploy to GCP project mlab-oti
+  - provider: script
+    script: "$TRAVIS_BUILD_DIR/deploy_snmp_exporter.sh mlab-oti"
+    skip_cleanup: true
+    on:
+      repo: m-lab/prometheus-snmp-exporter
+      tags: true

Dockerfile
@@ -1,0 +1,3 @@
+FROM prom/snmp-exporter:v0.6.0
+MAINTAINER support@measurementlab.net
+ADD mlab.yml /etc/snmp_exporter/snmp.yml

build
@@ -1,0 +1,7 @@
+#!/bin/bash
+
+# Exit build script on first failure.
+set -e
+
+git-hooks/python-pre-commit
+git-hooks/python-prepare-commit-msg

deploy_snmp_exporter.sh
@@ -1,0 +1,67 @@
+#!/bin/bash
+
+set -e
+set -u
+set -x
+
+# These variables should not change much
+USAGE="Usage: $0 <project>"
+PROJECT=${1:?Please provide project name: $USAGE}
+CREDS_FILE="snmp-exporter-service-account_${PROJECT}.json"
+SCP_FILES="Dockerfile mlab.yml"
+IMAGE_TAG="m-lab/prometheus-snmp-exporter"
+GCE_ZONE="us-central1-a"
+GCE_NAME="snmp-exporter"
+GCE_IP_NAME="snmp-exporter-public-ip"
+GCE_IMG_PROJECT="cos-cloud"
+GCE_IMG_FAMILY="cos-stable"
+
+# Add gcloud to PATH.
+source "${HOME}/google-cloud-sdk/path.bash.inc"
+
+# Generate the snmp_exporter configuration file.
+$TRAVIS_BUILD_DIR/gen_snmp_exporter_config.py
+
+# Set the project and zone for all future gcloud commands.
+gcloud config set project $PROJECT
+gcloud config set compute/zone $GCE_ZONE
+
+# Authenticate the service account using the JSON credentials file.
+if [[ -f "/tmp/${CREDS_FILE}" ]] ; then
+  gcloud auth activate-service-account --key-file /tmp/$CREDS_FILE
+else
+  echo "Service account credentials not found at /tmp/${CREDS_FILE}!"
+  exit 1
+fi
+
+# Make sure that the files we want to copy actually exist.
+for scp_file in ${SCP_FILES}; do
+  if [[ ! -f "${TRAVIS_BUILD_DIR}/${scp_file}" ]]; then
+    echo "Missing required file: ${TRAVIS_BUILD_DIR}/${scp_file}!"
+    exit 1
+  fi
+done
+
+# Delete the existing GCE instance, if it exists. gcloud has an exit status of 0
+# whether any instances are found or not. When no instances are found, a short
+# message is echoed to stderr. When an instance is found a summary is echoed to
+# stdout. If $EXISTING_INSTANCE is not null then we infer that the instance
+# already exists.
+EXISTING_INSTANCE=$(gcloud compute instances list --filter "name=${GCE_NAME}")
+if [[ -n "${EXISTING_INSTANCE}" ]]; then
+  gcloud compute instances delete $GCE_NAME --quiet
+fi
+
+# Create the new GCE instance. NOTE: $GCE_IP_NAME *must* refer to an existing
+# static external IP address for the project.
+gcloud compute instances create $GCE_NAME --address $GCE_IP_NAME \
+  --image-project $GCE_IMG_PROJECT --image-family $GCE_IMG_FAMILY
+
+# Copy required snmp_exporter files to the GCE instance.
+gcloud compute scp $SCP_FILES $GCE_NAME:~
+
+# Build the snmp_exporter Docker container.
+gcloud compute ssh $GCE_NAME --command "docker build -t ${IMAGE_TAG} ."
+
+# Start a new container based on the new/updated image
+gcloud compute ssh $GCE_NAME --command "docker run -p 9116:9116 -d ${IMAGE_TAG}"

gen_snmp_exporter_config.py
@@ -1,0 +1,138 @@
+#!/usr/bin/env python
+"""Generate an snmp_exporter configuration file for M-Lab's Juniper switches."""
+
+# Copyright 2017 Measurement Lab
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import json
+import logging
+import string
+import sys
+
+
+def parse_options(args):
+    """Parses the options passed to this script.
+
+    Args:
+        args: list of options passed on command line
+
+    Returns:
+        An argparse.ArgumentParser object containing the passed options and
+        their values.
+    """
+    parser = argparse.ArgumentParser(
+        description='Generates an snmp_exporter config M-Lab QFX5100 switches.')
+    parser.add_argument('--output_file',
+                        dest='output_file',
+                        default='mlab.yml',
+                        help='Filename where output will be written.')
+    parser.add_argument('--juniper_template_path',
+                        dest='juniper_template_path',
+                        default='juniper-snmp_exporter-template',
+                        help='Path to the Juniper config template file.')
+    parser.add_argument('--other_template_path',
+                        dest='other_template_path',
+                        default='other-snmp_exporter-template',
+                        help='Path to config template file for HPs and Ciscos.')
+
+    args = parser.parse_args(args)
+
+    return args
+
+
+# TODO: the contents of this function are hopefully temporary. At the time of
+# this writing, M-Lab does not have a centralized data store where site
+# configuration data can be stored, retrieved and updated. In this particular
+# example, the current canonical source of information for default switch SNMP
+# communities and uplink ports is a file (switch-details.json) in the
+# switch-config repository. In a better future, this function will consult some
+# sort of centralized data store.
+def read_switch_details():  # pragma: no cover
+    """Retrieves switch details.
+
+    Returns: A dict with all Juniper switch details
+    """
+    switch_details_path = 'switch-config/switch-details.json'
+
+    with open(switch_details_path, 'r') as details_file:
+        switch_details = json.load(details_file)
+
+    return switch_details
+
+
+def generate_site_config(site, details, config_template):
+    """Generates an snmp_exporter config section for a given site.
+
+    Args:
+        site: str, short name of site (e.g., abc01).
+        details: dict, switch configuration details.
+        config_template: str, the string template for the switch.
+
+    Returns:
+        str, a valid snmp_exporter configuration for the passed site.
+    """
+    template = string.Template(config_template)
+
+    template_vars = {'site': site, 'community': details['community']}
+
+    try:
+        site_config = template.substitute(template_vars)
+    except KeyError, err:
+        logging.error(err)
+        raise
+
+    return site_config
+
+
+def write_config(switch_details, templates, output):
+    """Writes configs for each site to a single config file.
+
+    Args:
+        switch_details: dict, all switch configuration details.
+        templates: dict, string templates for various switch types.
+        output: file, file handle to write output to.
+
+    """
+    for site, details in sorted(switch_details.iteritems()):
+        if details['switch_make'] == 'juniper':
+            config_template = templates['juniper']
+        else:
+            config_template = templates['other']
+
+        site_config = generate_site_config(site, details, config_template)
+        output.write(site_config)
+
+
+def main(argv):
+    """Main."""
+    args = parse_options(argv[1:])
+    switch_details = read_switch_details()
+    exporter_config_file = open(args.output_file, 'w')
+
+    templates = {}
+    try:
+        templates['juniper'] = open(args.juniper_template_path, 'r').read()
+        templates['other'] = open(args.other_template_path, 'r').read()
+    except IOError, err:
+        logging.error(err)
+        raise
+
+    write_config(switch_details, templates, exporter_config_file)
+
+    exporter_config_file.close()
+
+
+if __name__ == '__main__':  # pragma: no cover
+    main(sys.argv)

juniper-snmp_exporter-template
@@ -1,0 +1,95 @@
+${site}:
+  version: 2
+  auth:
+    community: ${community}
+  walk:
+  - 1.3.6.1.2.1.2
+  - 1.3.6.1.2.1.31.1.1
+  - 1.3.6.1.4.1.2636.3.15.4.1
+  metrics:
+  - name: jnxCosQstatTotalDropPkts
+    oid: 1.3.6.1.4.1.2636.3.15.4.1.53
+    type: counter
+    indexes:
+    - labelname: ifIndex
+      type: gauge
+    - labelname: qNumber
+      type: gauge
+    lookups:
+    - labels:
+      - ifIndex
+      labelname: ifDescr
+      oid: 1.3.6.1.2.1.2.2.1.2
+      type: DisplayString
+  - name: ifInErrors
+    oid: 1.3.6.1.2.1.2.2.1.14
+    type: counter
+    indexes:
+    - labelname: ifIndex
+      type: gauge
+    lookups:
+    - labels:
+      - ifIndex
+      labelname: ifDescr
+      oid: 1.3.6.1.2.1.2.2.1.2
+      type: DisplayString
+  - name: ifOutErrors
+    oid: 1.3.6.1.2.1.2.2.1.20
+    type: counter
+    indexes:
+    - labelname: ifIndex
+      type: gauge
+    lookups:
+    - labels:
+      - ifIndex
+      labelname: ifDescr
+      oid: 1.3.6.1.2.1.2.2.1.2
+      type: DisplayString
+  - name: ifHCInOctets
+    oid: 1.3.6.1.2.1.31.1.1.1.6
+    type: counter
+    indexes:
+    - labelname: ifIndex
+      type: gauge
+    lookups:
+    - labels:
+      - ifIndex
+      labelname: ifDescr
+      oid: 1.3.6.1.2.1.2.2.1.2
+      type: DisplayString
+  - name: ifHCOutOctets
+    oid: 1.3.6.1.2.1.31.1.1.1.10
+    type: counter
+    indexes:
+    - labelname: ifIndex
+      type: gauge
+    lookups:
+    - labels:
+      - ifIndex
+      labelname: ifDescr
+      oid: 1.3.6.1.2.1.2.2.1.2
+      type: DisplayString
+  - name: ifHCInUcastPkts
+    oid: 1.3.6.1.2.1.31.1.1.1.7
+    type: counter
+    indexes:
+    - labelname: ifIndex
+      type: gauge
+    lookups:
+    - labels:
+      - ifIndex
+      labelname: ifDescr
+      oid: 1.3.6.1.2.1.2.2.1.2
+      type: DisplayString
+  - name: ifHCOutUcastPkts
+    oid: 1.3.6.1.2.1.31.1.1.1.11
+    type: counter
+    indexes:
+    - labelname: ifIndex
+      type: gauge
+    lookups:
+    - labels:
+      - ifIndex
+      labelname: ifDescr
+      oid: 1.3.6.1.2.1.2.2.1.2
+      type: DisplayString

other-snmp_exporter-template
@@ -1,0 +1,105 @@
+${site}:
+  version: 2
+  auth:
+    community: ${community}
+  walk:
+  - 1.3.6.1.2.1.2
+  - 1.3.6.1.2.1.31.1.1
+  - 1.3.6.1.4.1.2636.3.15.4.1
+  metrics:
+  - name: ifInDiscards
+    oid: 1.3.6.1.2.1.2.2.1.13
+    type: counter
+    indexes:
+    - labelname: ifIndex
+      type: gauge
+    lookups:
+    - labels:
+      - ifIndex
+      labelname: ifDescr
+      oid: 1.3.6.1.2.1.2.2.1.2
+      type: DisplayString
+  - name: ifOutDiscards
+    oid: 1.3.6.1.2.1.2.2.1.19
+    type: counter
+    indexes:
+    - labelname: ifIndex
+      type: gauge
+    lookups:
+    - labels:
+      - ifIndex
+      labelname: ifDescr
+      oid: 1.3.6.1.2.1.2.2.1.2
+      type: DisplayString
+  - name: ifInErrors
+    oid: 1.3.6.1.2.1.2.2.1.14
+    type: counter
+    indexes:
+    - labelname: ifIndex
+      type: gauge
+    lookups:
+    - labels:
+      - ifIndex
+      labelname: ifDescr
+      oid: 1.3.6.1.2.1.2.2.1.2
+      type: DisplayString
+  - name: ifOutErrors
+    oid: 1.3.6.1.2.1.2.2.1.20
+    type: counter
+    indexes:
+    - labelname: ifIndex
+      type: gauge
+    lookups:
+    - labels:
+      - ifIndex
+      labelname: ifDescr
+      oid: 1.3.6.1.2.1.2.2.1.2
+      type: DisplayString
+  - name: ifHCInOctets
+    oid: 1.3.6.1.2.1.31.1.1.1.6
+    type: counter
+    indexes:
+    - labelname: ifIndex
+      type: gauge
+    lookups:
+    - labels:
+      - ifIndex
+      labelname: ifDescr
+      oid: 1.3.6.1.2.1.2.2.1.2
+      type: DisplayString
+  - name: ifHCOutOctets
+    oid: 1.3.6.1.2.1.31.1.1.1.10
+    type: counter
+    indexes:
+    - labelname: ifIndex
+      type: gauge
+    lookups:
+    - labels:
+      - ifIndex
+      labelname: ifDescr
+      oid: 1.3.6.1.2.1.2.2.1.2
+      type: DisplayString
+  - name: ifHCInUcastPkts
+    oid: 1.3.6.1.2.1.31.1.1.1.7
+    type: counter
+    indexes:
+    - labelname: ifIndex
+      type: gauge
+    lookups:
+    - labels:
+      - ifIndex
+      labelname: ifDescr
+      oid: 1.3.6.1.2.1.2.2.1.2
+      type: DisplayString
+  - name: ifHCOutUcastPkts
+    oid: 1.3.6.1.2.1.31.1.1.1.11
+    type: counter
+    indexes:
+    - labelname: ifIndex
+      type: gauge
+    lookups:
+    - labels:
+      - ifIndex
+      labelname: ifDescr
+      oid: 1.3.6.1.2.1.2.2.1.2
+      type: DisplayString

gen_snmp_exporter_config_test.py
@@ -1,0 +1,286 @@
+#!/usr/bin/env python
+#
+# Copyright 2017 Measurement Lab
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# No docstrings required for tests, and tests need to be methods of classes to
+# aid in organization of tests. Using the 'self' variable is not required.
+#
+# pylint: disable=missing-docstring, no-self-use, too-many-public-methods
+
+import json
+import logging
+import os
+import textwrap
+import unittest
+
+import gen_snmp_exporter_config
+import mock
+
+logging.disable(logging.CRITICAL)
+
+
+class GenSnmpExporterConfigTest(unittest.TestCase):
+
+    def setUp(self):
+
+        self.output_file = '/tmp/test_gen_snmp_exporter_config'
+        self.argv_good = ['rofl', '--output_file', self.output_file]
+        self.argv_bad = ['rofl', '--juniper_template_path', '/lol/not_here.txt']
+        self.sample_details_json = """{
+            "lol01": {
+                "auto_negotiation": "yes",
+                "community": "ekEa00ClZZkTvEJqe0jU",
+                "switch_make": "hp",
+                "uplink_port": "48",
+                "uplink_speed": "1g"
+            },
+            "brb02": {
+                "auto_negotiation": "no",
+                "community": "tyUenN0gEVF0LdbtbQmH",
+                "switch_make": "juniper",
+                "uplink_port": "45",
+                "uplink_speed": "10g"
+            }
+        }"""
+
+        self.expected_output = textwrap.dedent("""\
+            brb02:
+              version: 2
+              auth:
+                community: tyUenN0gEVF0LdbtbQmH
+              walk:
+              - 1.3.6.1.2.1.2
+              - 1.3.6.1.2.1.31.1.1
+              - 1.3.6.1.4.1.2636.3.15.4.1
+              metrics:
+              - name: jnxCosQstatTotalDropPkts
+                oid: 1.3.6.1.4.1.2636.3.15.4.1.53
+                type: counter
+                indexes:
+                - labelname: ifIndex
+                  type: gauge
+                - labelname: qNumber
+                  type: gauge
+                lookups:
+                - labels:
+                  - ifIndex
+                  labelname: ifDescr
+                  oid: 1.3.6.1.2.1.2.2.1.2
+                  type: DisplayString
+              - name: ifInErrors
+                oid: 1.3.6.1.2.1.2.2.1.14
+                type: counter
+                indexes:
+                - labelname: ifIndex
+                  type: gauge
+                lookups:
+                - labels:
+                  - ifIndex
+                  labelname: ifDescr
+                  oid: 1.3.6.1.2.1.2.2.1.2
+                  type: DisplayString
+              - name: ifOutErrors
+                oid: 1.3.6.1.2.1.2.2.1.20
+                type: counter
+                indexes:
+                - labelname: ifIndex
+                  type: gauge
+                lookups:
+                - labels:
+                  - ifIndex
+                  labelname: ifDescr
+                  oid: 1.3.6.1.2.1.2.2.1.2
+                  type: DisplayString
+              - name: ifHCInOctets
+                oid: 1.3.6.1.2.1.31.1.1.1.6
+                type: counter
+                indexes:
+                - labelname: ifIndex
+                  type: gauge
+                lookups:
+                - labels:
+                  - ifIndex
+                  labelname: ifDescr
+                  oid: 1.3.6.1.2.1.2.2.1.2
+                  type: DisplayString
+              - name: ifHCOutOctets
+                oid: 1.3.6.1.2.1.31.1.1.1.10
+                type: counter
+                indexes:
+                - labelname: ifIndex
+                  type: gauge
+                lookups:
+                - labels:
+                  - ifIndex
+                  labelname: ifDescr
+                  oid: 1.3.6.1.2.1.2.2.1.2
+                  type: DisplayString
+              - name: ifHCInUcastPkts
+                oid: 1.3.6.1.2.1.31.1.1.1.7
+                type: counter
+                indexes:
+                - labelname: ifIndex
+                  type: gauge
+                lookups:
+                - labels:
+                  - ifIndex
+                  labelname: ifDescr
+                  oid: 1.3.6.1.2.1.2.2.1.2
+                  type: DisplayString
+              - name: ifHCOutUcastPkts
+                oid: 1.3.6.1.2.1.31.1.1.1.11
+                type: counter
+                indexes:
+                - labelname: ifIndex
+                  type: gauge
+                lookups:
+                - labels:
+                  - ifIndex
+                  labelname: ifDescr
+                  oid: 1.3.6.1.2.1.2.2.1.2
+                  type: DisplayString
+            lol01:
+              version: 2
+              auth:
+                community: ekEa00ClZZkTvEJqe0jU
+              walk:
+              - 1.3.6.1.2.1.2
+              - 1.3.6.1.2.1.31.1.1
+              - 1.3.6.1.4.1.2636.3.15.4.1
+              metrics:
+              - name: ifInDiscards
+                oid: 1.3.6.1.2.1.2.2.1.13
+                type: counter
+                indexes:
+                - labelname: ifIndex
+                  type: gauge
+                lookups:
+                - labels:
+                  - ifIndex
+                  labelname: ifDescr
+                  oid: 1.3.6.1.2.1.2.2.1.2
+                  type: DisplayString
+              - name: ifOutDiscards
+                oid: 1.3.6.1.2.1.2.2.1.19
+                type: counter
+                indexes:
+                - labelname: ifIndex
+                  type: gauge
+                lookups:
+                - labels:
+                  - ifIndex
+                  labelname: ifDescr
+                  oid: 1.3.6.1.2.1.2.2.1.2
+                  type: DisplayString
+              - name: ifInErrors
+                oid: 1.3.6.1.2.1.2.2.1.14
+                type: counter
+                indexes:
+                - labelname: ifIndex
+                  type: gauge
+                lookups:
+                - labels:
+                  - ifIndex
+                  labelname: ifDescr
+                  oid: 1.3.6.1.2.1.2.2.1.2
+                  type: DisplayString
+              - name: ifOutErrors
+                oid: 1.3.6.1.2.1.2.2.1.20
+                type: counter
+                indexes:
+                - labelname: ifIndex
+                  type: gauge
+                lookups:
+                - labels:
+                  - ifIndex
+                  labelname: ifDescr
+                  oid: 1.3.6.1.2.1.2.2.1.2
+                  type: DisplayString
+              - name: ifHCInOctets
+                oid: 1.3.6.1.2.1.31.1.1.1.6
+                type: counter
+                indexes:
+                - labelname: ifIndex
+                  type: gauge
+                lookups:
+                - labels:
+                  - ifIndex
+                  labelname: ifDescr
+                  oid: 1.3.6.1.2.1.2.2.1.2
+                  type: DisplayString
+              - name: ifHCOutOctets
+                oid: 1.3.6.1.2.1.31.1.1.1.10
+                type: counter
+                indexes:
+                - labelname: ifIndex
+                  type: gauge
+                lookups:
+                - labels:
+                  - ifIndex
+                  labelname: ifDescr
+                  oid: 1.3.6.1.2.1.2.2.1.2
+                  type: DisplayString
+              - name: ifHCInUcastPkts
+                oid: 1.3.6.1.2.1.31.1.1.1.7
+                type: counter
+                indexes:
+                - labelname: ifIndex
+                  type: gauge
+                lookups:
+                - labels:
+                  - ifIndex
+                  labelname: ifDescr
+                  oid: 1.3.6.1.2.1.2.2.1.2
+                  type: DisplayString
+              - name: ifHCOutUcastPkts
+                oid: 1.3.6.1.2.1.31.1.1.1.11
+                type: counter
+                indexes:
+                - labelname: ifIndex
+                  type: gauge
+                lookups:
+                - labels:
+                  - ifIndex
+                  labelname: ifDescr
+                  oid: 1.3.6.1.2.1.2.2.1.2
+                  type: DisplayString\n""")
+
+    @mock.patch.object(gen_snmp_exporter_config, 'read_switch_details')
+    def test_main(self, mock_read_switch_details):
+        mock_read_switch_details.return_value = json.loads(
+            self.sample_details_json)
+        gen_snmp_exporter_config.main(self.argv_good)
+        actual_output = open(self.output_file).read()
+        os.remove(self.output_file)
+        self.assertEqual(actual_output, self.expected_output)
+
+    def test_main_bad_template_path_raises_ioerror(self):
+        with self.assertRaises(IOError):
+            gen_snmp_exporter_config.main(self.argv_bad)
+
+    @mock.patch.object(gen_snmp_exporter_config, 'read_switch_details')
+    def test_generate_site_config_bad_tmpl_raises_keyerror(
+            self, mock_read_switch_details):
+        mock_read_switch_details.return_value = json.loads(
+            self.sample_details_json)
+        switch_details = gen_snmp_exporter_config.read_switch_details()
+        some_detail = switch_details.popitem()
+        with self.assertRaises(KeyError):
+            gen_snmp_exporter_config.generate_site_config(
+                some_detail[0], some_detail[1], '${lol}')
+
+
+if __name__ == '__main__':  # pragma: no cover
+    unittest.main()